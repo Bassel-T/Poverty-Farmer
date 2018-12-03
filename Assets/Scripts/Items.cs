9d578f434751b34a05cfbbf469aa2aa2c9c57216
Mightymaster99 <mightymaster99@users.noreply.github.com>
2018-12-02 19:20:37 -0600

    Initial commit

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items {
+
+    public string itemName;
+    public int index;
+    public int price;
+    public string type;
+    public GameObject prefab;
+    public double multiplier;
+
+    public Items(string itemName, int index, int price, string type, GameObject prefab, double multiplier) {
+        this.itemName = itemName;
+        this.index = index;
+        this.price = price;
+        this.type = type;
+        this.prefab = prefab;
+        this.multiplier = multiplier;
+    }
+
+}
---
+++ Assets/Scripts/Tiles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour {
+
+    public bool active;
+    public int ownedIndex;
+
+    private void Start() {
+        active = false;
+        ownedIndex = -1;
+    }
+
+    private void OnMouseDown() {
+        if (active) {
+            GameObject.Find("EventSystem").GetComponent<Game>().Place(this.gameObject);
+                
+        }
+    }
+}

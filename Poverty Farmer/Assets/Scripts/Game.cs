c1c3d3afc3a7fd020a02ca9f8f0488502568cf6c
Mightymaster99 <mightymaster99@users.noreply.github.com>
2018-12-02 19:24:23 -0600

    Initial Commit

---
+++ Poverty Farmer/Assets/Scripts/Game.cs
@@ -0,0 +1,261 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+    public GameObject PlantShop, AnimalShop, ToolShop;
+    public GameObject plantButton, animalButton, toolButton;
+    public GameObject plantArrow, animalArrow, toolArrow;
+    public GameObject[] clickableTiles;
+    public GameObject balanceText, turnText;
+    public GameObject nextYearCanvas;
+
+    public GameObject cornPrefab, wheatPrefab, cottonPrefab, peanutPrefab, chickenPrefab, pigPrefab, cowPrefab, horsePrefab, shovelPrefab, scythePrefab, tractorPrefab;
+
+    public List<Items> items;
+
+    public int itemIndex;
+    public int balance;
+    public int yearlyMultiplier;
+    public int turn;
+
+    private void Start() {
+        balance = 50;
+        yearlyMultiplier = 1;
+        //Need to set up the initial variables.
+        PlantShop = GameObject.Find("PlantShop");
+        AnimalShop = GameObject.Find("AnimalShop");
+        ToolShop = GameObject.Find("ToolShop");
+        PlantShop.SetActive(false);
+        AnimalShop.SetActive(false);
+        ToolShop.SetActive(false);
+
+        plantButton = GameObject.Find("Plant Button");
+        animalButton = GameObject.Find("Animal Button");
+        toolButton = GameObject.Find("Tool Button");
+        plantArrow = GameObject.Find("Plant Arrow");
+        animalArrow = GameObject.Find("Animal Arrow");
+        toolArrow = GameObject.Find("Tool Arrow");
+        nextYearCanvas = GameObject.Find("NextYearCanvas");
+
+        nextYearCanvas.SetActive(false);
+        plantArrow.SetActive(false);
+        animalArrow.SetActive(false);
+        toolArrow.SetActive(false);
+
+        balanceText = GameObject.Find("Balance Text");
+        turnText = GameObject.Find("Turn Text");
+
+        clickableTiles = GameObject.FindGameObjectsWithTag("Tiles");
+        foreach (GameObject tile in clickableTiles) {
+                tile.GetComponent<Tiles>().active = false;
+        }
+        
+        items = new List<Items> {
+            new Items("Corn", 0, 8 ,"Plant", cornPrefab, 0),
+            new Items("Wheat", 1, 13, "Plant", wheatPrefab, 0),
+            new Items("Cotton", 2, 17, "Plant", cottonPrefab, 0),
+            new Items("Peanut", 3, 21, "Plant", peanutPrefab, 0),
+            new Items("Chicken", 4, 25, "Animal", chickenPrefab, 0),
+            new Items("Pig", 5, 50, "Animal", pigPrefab, 0),
+            new Items("Cow", 6, 100, "Animal", cowPrefab, 0),
+            new Items("Horse", 7, 300, "Animal", horsePrefab, 0),
+            new Items("Shovel", 8, 50, "Tool", shovelPrefab, 0.1),
+            new Items("Scythe", 9, 100, "Tool", scythePrefab, 0.5),
+            new Items("Tractor", 10, 300, "Tool", tractorPrefab, 1)
+        };
+    }
+
+    // Button to show the plant menu.
+    public void PlantMenu() {
+        // Close other menus.
+        AnimalShop.SetActive(false);
+        ToolShop.SetActive(false);
+        // Open Planet Menu.
+        PlantShop.SetActive(true);
+        plantArrow.SetActive(true);
+        animalArrow.SetActive(false);
+        toolArrow.SetActive(false);
+    }
+
+
+    // Button to show the tool menu.
+    public void ToolMenu() {
+        // Close other menus.
+        AnimalShop.SetActive(false);
+        PlantShop.SetActive(false);
+        //Open Tool Menu.
+        ToolShop.SetActive(true);
+        plantArrow.SetActive(false);
+        animalArrow.SetActive(false);
+        toolArrow.SetActive(true);
+    }
+
+
+    // Button to show the animal menu.
+    public void AnimalMenu() {
+        // Close other menus.
+        PlantShop.SetActive(false);
+        ToolShop.SetActive(false);
+        // Open Animal Menu.
+        AnimalShop.SetActive(true);
+        plantArrow.SetActive(false);
+        animalArrow.SetActive(true);
+        toolArrow.SetActive(false);
+    }
+
+
+    // Button to choose what's being placed.
+    public void Choice(int index) {
+        // Check if user can afford the item.
+        if (balance >= items[index].price * yearlyMultiplier) {
+            // Set a variable to a specific item based on the index.
+            itemIndex = index;
+
+            // Set up an arrow that will inform of the right tile. ???
+
+
+            // Make the tiles clickable.
+            foreach (GameObject tile in clickableTiles) {
+                if (tile.name.StartsWith(items[itemIndex].type)) {
+                    tile.GetComponent<Tiles>().active = true;
+                } else {
+                    tile.GetComponent<Tiles>().active = false;
+                }
+            }
+        }
+    }
+
+
+    // Button to place down the item.
+    public void Place(GameObject foo) {
+        //Check if the tile is the proper tile for that.
+        if (foo.name == items[itemIndex].type.ToString() + "Tile" && (balance >= items[itemIndex].price)) {
+            GameObject newItem = (GameObject)Instantiate(items[itemIndex].prefab, foo.transform, instantiateInWorldSpace: false);
+            newItem.transform.localPosition = new Vector3(0, 0, 0);
+            balance -= items[itemIndex].price * yearlyMultiplier;
+            balanceText.GetComponent<Text>().text = "Balance: $" + balance.ToString();
+            foo.GetComponent<Tiles>().ownedIndex = itemIndex;
+        }
+    }
+
+
+    // Next year button.
+    public void NextYear() {
+        // Set everything not-interactable.
+        foreach (GameObject tile in clickableTiles) {
+            tile.GetComponent<Tiles>().active = false;
+        }
+        plantArrow.SetActive(false);
+        animalArrow.SetActive(false);
+        toolArrow.SetActive(false);
+        // Do crazy math.
+        int baseRevenue = 0;
+        int money = 0;
+        double multiply = 0;
+
+        // Add up the revenue gained from each of the items (though slightly randomized) as well as the multiplier from tools.
+        foreach (GameObject tile in clickableTiles) {
+            if (tile.GetComponent<Tiles>().ownedIndex >= 0) {
+                if (items[tile.GetComponent<Tiles>().ownedIndex].type == "Plant" || items[tile.GetComponent<Tiles>().ownedIndex].type == "Animal") {
+                    money += (int)(items[tile.GetComponent<Tiles>().ownedIndex].price + (2 * Mathf.Sin(Random.Range(0, 2 * Mathf.PI))));
+                } else if (items[tile.GetComponent<Tiles>().ownedIndex].type == "Tool") {
+                    multiply += items[tile.GetComponent<Tiles>().ownedIndex].multiplier;
+                }
+            }
+        }
+        // Set the base revenue from what you have.
+        baseRevenue = (int)(money * (1 + multiply));
+
+        double familyEfficiency = 0;
+
+        // Read how efficient the family is. (Allowed to be over 100%).
+        foreach (Human child in GameObject.Find("Family").transform.GetComponentsInChildren<Human>()) {
+            familyEfficiency += child.annualOutput;
+        }
+        // Gives the family income.
+        int income = (int)(baseRevenue * familyEfficiency);
+
+        //Calculate the living cost.
+
+        int livingCost = 0;
+        foreach (GameObject tile in clickableTiles) {
+            if (tile.GetComponent<Tiles>().ownedIndex >= 0) {
+                if (items[tile.GetComponent<Tiles>().ownedIndex].type == "Tool") {
+                    livingCost += (int)((items[tile.GetComponent<Tiles>().ownedIndex].price / 5) + (2 * Mathf.Sin(Random.Range(0, 2 * Mathf.PI))));
+                }
+            }
+        }
+        livingCost += GameObject.Find("Family").transform.childCount * 7;
+
+        int savings = balance;
+        balance += income - livingCost;
+        foreach (GameObject tile in clickableTiles) {
+            if (tile.GetComponent<Tiles>().ownedIndex >= 0) {
+                if (items[tile.GetComponent<Tiles>().ownedIndex].type == "Plant") {
+                    Destroy(tile.transform.GetChild(0).gameObject);
+                    tile.GetComponent<Tiles>().ownedIndex = -1;
+                } else if (items[tile.GetComponent<Tiles>().ownedIndex].type == "Animal") {
+                    int value = Random.Range(0, balance);
+                    if (value < (balance / 1.5f)) {
+                        Destroy(tile.transform.GetChild(0).gameObject);
+                        tile.GetComponent<Tiles>().ownedIndex = -1;
+                    }
+                } else if (items[tile.GetComponent<Tiles>().ownedIndex].type == "Tool") {
+                    int value = Random.Range(0, balance);
+                    if (value < (balance / 2.5f)) {
+                        Destroy(tile.transform.GetChild(0).gameObject);
+                        tile.GetComponent<Tiles>().ownedIndex = -1;
+                    }
+                }
+            }
+        }
+
+        nextYearCanvas.SetActive(true);
+        GameObject.Find("MoneyFromItems").GetComponent<Text>().text = "Item Revenue: $" + money.ToString();
+        GameObject.Find("MultiplierFromTools").GetComponent<Text>().text = "Tool Multiplier: x" + (1 + multiply).ToString();
+        GameObject.Find("BaseRevenue").GetComponent<Text>().text = "Base Revenue: $" + baseRevenue.ToString();
+        GameObject.Find("FamilyEfficiency").GetComponent<Text>().text = "Family Efficiency: " + (Mathf.Round((float)(familyEfficiency * 100))).ToString() + "%";
+        GameObject.Find("RealIncome").GetComponent<Text>().text = "Household Income: $" + income.ToString();
+        GameObject.Find("LivingCost").GetComponent<Text>().text = "Cost of Living: $" + livingCost.ToString();
+        GameObject.Find("Savings").GetComponent<Text>().text = "Past Savings: $" + savings.ToString();
+        GameObject.Find("NewBalance").GetComponent<Text>().text = "New Balance: $" + balance.ToString();
+
+        foreach (Human child in GameObject.Find("Family").GetComponentsInChildren<Human>()) {
+            if (child.health < 100) {
+                Destroy(child.gameObject);
+            }
+        }
+
+        if (GameObject.Find("Family").transform.childCount == 0) {
+            GameObject.Find("Continue!").transform.GetChild(0).GetComponent<Text>().text = "Everyone died! Try again?";
+        }
+
+        if (balance <= 0) {
+            GameObject.Find("Continue!").transform.GetChild(0).GetComponent<Text>().text = "No more money! Try again?";
+        }
+
+    }
+
+
+
+        // Button to close the "Next Level" thing.
+        public void CloseNext() {
+        // Close next menu.
+        turn += 1;
+        turnText.GetComponent<Text>().text = "Turn: " + turn.ToString();
+        balanceText.GetComponent<Text>().text = "Balance: $" + balance.ToString();
+        GameObject.Find("Continue!").transform.GetChild(0).GetComponent<Text>().text = "Continue!";
+        nextYearCanvas.SetActive(false);
+        foreach (Human child in GameObject.Find("Family").GetComponentsInChildren<Human>()) {
+            child.newYear();
+        }
+
+        if (GameObject.Find("Family").transform.childCount == 0 || balance <= 0) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+        }
+
+    }
+
+}
---
+++ Poverty Farmer/Assets/Scripts/Human.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Human : MonoBehaviour {
+
+    // Traits
+    public bool male; // If true, male. If false, female. For babies.
+    public bool fertile;
+    public bool pergenat; // Is the lady pregnant? -.-
+    public bool userGenerated;
+
+    // Factors of Efficiency
+    public double educatedProgress; // A measurement of their education. From 0-10.
+    public bool goingToSchool;
+    public int age;
+    public double health; //HP out of 100.
+
+    // Final Efficiencies
+    public double efficiency; //Efficiency, multiple of 1. (Just in case you need it)
+    public double annualOutput; // A measurement of how much they are able to output in a given time period.
+
+    // Miscellaneous variables.
+    public Game script;
+    public GameObject personCanvas;
+    public GameObject maleChild, femaleChild;
+
+    private void Start() {
+        // Getting the script so I can setup the balance for later.
+        script = GameObject.Find("EventSystem").GetComponent<Game>();
+
+        // Set up for other things
+        health = 100;
+        goingToSchool = false;
+        fertile = false;
+
+        if (userGenerated) {
+            age = 0;
+            educatedProgress = 0;
+        }
+
+        annualOutput = resultingOutput(AgeToEfficiency(age), healthToEfficiency(health), educationToEfficiency(educatedProgress));
+
+        // Getting the children so that I can get the buttons working properly.
+        personCanvas = this.gameObject.transform.GetChild(0).gameObject;
+        personCanvas.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(() => SellPerson());
+        personCanvas.transform.GetChild(2).GetComponent<Button>().onClick.AddListener(() => SendToSchool());
+        personCanvas.transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() => HealPerson());
+        if (!male) {
+            personCanvas.transform.GetChild(4).GetComponent<Button>().onClick.AddListener(() => GiveBirth());
+            personCanvas.transform.GetChild(10).GetComponent<Text>().text = "Gender: Female";
+        } else {
+            personCanvas.transform.GetChild(4).GetChild(0).GetComponent<Text>().text = "He cannot make babies!";
+            personCanvas.transform.GetChild(10).GetComponent<Text>().text = "Gender: Male";
+        }
+        personCanvas.transform.GetChild(5).GetComponent<Button>().onClick.AddListener(() => CloseMenu());
+        personCanvas.transform.GetChild(6).GetComponent<Text>().text = "Age: " + age.ToString();
+        personCanvas.transform.GetChild(7).GetComponent<Text>().text = "Health: " + health.ToString();
+        personCanvas.transform.GetChild(8).GetComponent<Text>().text = "Education (Max 10): " + educatedProgress.ToString();
+        personCanvas.transform.GetChild(9).GetComponent<Text>().text = "Efficiency: " + Mathf.RoundToInt((float)annualOutput).ToString() + "%";
+        personCanvas.SetActive(false);
+
+    }
+
+    double AgeToEfficiency(int ageFactor) {
+        if (ageFactor <= 7 || ageFactor >= 53) {
+            return 0;
+        } else {
+            return -0.002 * Mathf.Pow((ageFactor - 30), 2) + 1; 
+        }
+    }
+
+    double healthToEfficiency(double healthFactor) {
+        if (healthFactor <= 0 || healthFactor > 100) {
+            return 0;
+        } else {
+            return healthFactor / 100;
+        }
+    }
+
+    double educationToEfficiency(double educationProgress) {
+        if (educationProgress < 2) {
+            return 1;
+        } else if (educationProgress < 4) {
+            return 1.05;
+        } else if (educationProgress < 6) {
+            return 1.1;
+        } else if (educationProgress < 8) {
+            return 1.15;
+        } else if (educationProgress < 10) {
+            return 1.2;
+        } else {
+            return 1.25;
+        }
+    }
+
+    double resultingOutput(double ageEff, double healthEff, double eduEff) {
+        return ageEff * healthEff * eduEff / 1.25;
+    }
+
+    double healthDrop(int ageValue) {
+        return 0.02 * Mathf.Pow(ageValue - 30, 2) + 1;
+    }
+
+    double SellPrice(double outputRate) {
+        return 20 * outputRate;
+    }
+    
+    // This is the new year
+    // You are probably looking for this function/
+    // It is right here
+    // and waiting for you.
+    public void newYear() {
+        age += 1;
+        health -= healthDrop(age);
+        if (goingToSchool) {
+            educatedProgress += 1;
+            goingToSchool = false;
+        }
+
+        if (age >= 15) {
+            fertile = true;
+        }
+
+        if (health <= 0) {
+            Destroy(this.gameObject);
+        }
+
+        if (pergenat) {
+            pergenat = false;
+            // Create another child.
+            bool newChildIsMale = (Random.Range(0, 1) == 1) ? true : false;
+            GameObject newChild = (GameObject)Instantiate(newChildIsMale ? maleChild : femaleChild, new Vector3(0, 0, GameObject.Find("Family").transform.childCount * 1.5f), Quaternion.identity, GameObject.Find("Family").transform);
+            newChild.GetComponent<Human>().userGenerated = true;
+        }
+        personCanvas.transform.GetChild(3).GetComponent<Button>().interactable = true;
+        annualOutput = resultingOutput(AgeToEfficiency(age), healthToEfficiency(health), educationToEfficiency(educatedProgress));
+    }
+
+    // Buttons from UI
+
+    private void OnMouseDown() {
+        personCanvas.SetActive(true);
+        personCanvas.transform.GetChild(5).GetComponent<Button>().onClick.AddListener(() => CloseMenu());
+        personCanvas.transform.GetChild(6).GetComponent<Text>().text = "Age: " + age.ToString();
+        personCanvas.transform.GetChild(7).GetComponent<Text>().text = "Health: " + health.ToString();
+        personCanvas.transform.GetChild(8).GetComponent<Text>().text = "Education (Max 10): " + educatedProgress.ToString();
+        personCanvas.transform.GetChild(9).GetComponent<Text>().text = "Efficiency: " + (annualOutput * 100).ToString() + "%";
+    }
+
+    public void SellPerson() {
+        script.balance += (int)SellPrice(annualOutput);
+        script.balanceText.GetComponent<Text>().text = "Balance: " + script.balance.ToString();
+        Destroy(this.gameObject);
+    }
+
+    public void SendToSchool() {
+        annualOutput = 0;
+        goingToSchool = true;
+        script.balance -= 15;
+        script.balanceText.GetComponent<Text>().text = "Balance: " + script.balance.ToString();
+
+    }
+
+    public void HealPerson() {
+        if (script.balance >= 20 && health < 100) {
+            this.gameObject.GetComponent<Human>().health += 20;
+            personCanvas.transform.GetChild(7).GetComponent<Text>().text = "Health: " + health.ToString();
+            script.balance -= 20;
+            script.balanceText.GetComponent<Text>().text = "Balance: " + script.balance.ToString();
+        }
+
+        if (health == 100) {
+            personCanvas.transform.GetChild(3).GetComponent<Button>().interactable = false;
+        }
+
+    }
+
+    public void GiveBirth() {
+        if (fertile) {
+            pergenat = true;
+        }
+    }
+
+    public void CloseMenu() {
+        personCanvas.SetActive(false);
+    }
+    
+}
